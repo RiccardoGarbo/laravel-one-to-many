94a82e0924cc3af8ff4b14b60ffeb7eb1c539208
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-25 16:57:03 +0100

    create type seeder, add type_id to projects table

--- database/migrations/2024_03_25_150652_add_type_id_to_projects_table.php
+++ database/migrations/2024_03_25_150652_add_type_id_to_projects_table.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\Type;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 
 return new class extends Migration
 {
@@ -12,7 +14,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->foreignId('type_id')->constrained();
+            $table->foreignIdFor(Type::class)->after('id')->nullable()->constrained()->nullOnDelete();
         });
     }
 
